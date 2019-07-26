64d94451b45a469b2e99c5152eb32ee71b95223f
shaozhonghua_ext <zhonghua.shao@zznode.com>
2019-07-26 16:07:57 +0800

    day two

--- app/app.route.js
+++ app/app.route.js
@@ -24,7 +24,57 @@ angular.module('app', ['ui.router'])
       })
   }]);
 
-angular.module("app").controller("homeCtrl", function ($scope, $rootScope) {
-  $scope.username = "duke"
-  $scope.age = 18
+angular.module("app").controller("homeCtrl", function ($scope, $http) {
+  //定义变量
+  $scope.username = "jane"
+  $scope.age = 16
+  $scope.count = 1
+  $scope.show = true
+  $scope.add = true
+  $scope.hide = false
+  $scope.fruit = ["apple", "banana"]
+  $scope.currentFruit = ""
+  $scope.datalist = [{
+    name: "duke",
+    age: 18
+  }, {
+    name: "jane",
+    age: 16
+  }, {
+    name: "jack",
+    age: 29
+  }]
+  angular.forEach($scope.datalist, function (each) {
+    if (each.age > 17) {
+      console.log(each)
+    }
+  })
+
+  //发送请求
+  $http.get("http://106.52.224.182:3001/api/lists?gender=1").success(function (res) {
+    console.log(res)
+  })
+  // $http({
+  //   method: "get",
+  //   url: "http://106.52.224.182:3001/api/lists?gender=1"
+  // }).then(function suc(res) {
+  //   console.log(res)
+  // })
+
+  //定义函数
+  $scope.toggleShow = function () {
+    $scope.show = !$scope.show
+  }
+  $scope.toggleAdd = function () {
+    $scope.add = !$scope.add
+  }
+  $scope.toggleHide = function () {
+    $scope.hide = !$scope.hide
+  }
+  $scope.countPlus = function () {
+    $scope.count++
+  }
+  $scope.toggleFruit = function () {
+    $scope.currentFruit = $scope.currentFruit == $scope.fruit[0] ? $scope.fruit[1] : $scope.fruit[0]
+  }
 })
